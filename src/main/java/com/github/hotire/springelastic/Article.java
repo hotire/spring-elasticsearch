dbc0038173ededb1e3543e0bc7387506d4a617c7
hotire <gngh0101@gmail.com>
2020-01-31 02:06:25 +0900

    Fix index name

--- src/main/java/com/github/hotire/springelastic/Article.java
+++ src/main/java/com/github/hotire/springelastic/Article.java
@@ -3,13 +3,9 @@ package com.github.hotire.springelastic;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
-import java.util.List;
 
 @Getter
-@Document(indexName = "blog", type = "article")
+@Document(indexName = "hotire", type = "article")
 public class Article {
 
     @Id
@@ -17,7 +13,8 @@ public class Article {
 
     private String title;
 
-    @Field(type = FieldType.Nested, includeInParent = true)
-    private List<Author> authors;
+    public Article(String title) {
+        this.title = title;
+    }
 
 }
