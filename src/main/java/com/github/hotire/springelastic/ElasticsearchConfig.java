3cc6fb5640bfcd67d8cdec339dd130b7d3c1954f
hotire <gngh0101@gmail.com>
2020-01-30 23:28:33 +0900

    Add config elastic serach

--- src/main/java/com/github/hotire/springelastic/ElasticsearchConfig.java
+++ src/main/java/com/github/hotire/springelastic/ElasticsearchConfig.java
@@ -1,9 +1,46 @@
 package com.github.hotire.springelastic;
 
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+
 
 @EnableElasticsearchRepositories(basePackageClasses = ElasticsearchConfig.class)
 @Configuration
 public class ElasticsearchConfig {
+
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.port}")
+    private int esPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String esClusterName;
+
+    @Bean
+    public Client client() throws Exception{
+        Settings esSettings = Settings.builder()
+                                      .put("cluster.name", esClusterName)
+                                      .build();
+
+        return new PreBuiltTransportClient(esSettings)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(esHost), esPort));
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws Exception{
+        return new ElasticsearchTemplate(client());
+    }
+
 }
