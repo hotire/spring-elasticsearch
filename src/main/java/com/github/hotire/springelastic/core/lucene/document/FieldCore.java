45fe85b5efd9ad3b2eb0fa6ef990669d87efdd77
hotire <gngh0101@gmail.com>
2022-07-19 00:15:17 +0900

    Add FieldCore

---
+++ src/main/java/com/github/hotire/springelastic/core/lucene/document/FieldCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springelastic.core.lucene.document;
+
+/**
+ * @see org.apache.lucene.index.IndexableField
+ * @see org.apache.lucene.document.Field
+ */
+public class FieldCore {
+}
