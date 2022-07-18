d128d709d7dcdf0368dab18f30dd6a525d58abf6
hotire <gngh0101@gmail.com>
2022-07-19 00:02:16 +0900

    Add DirectoryCore

---
+++ src/main/java/com/github/hotire/springelastic/core/lucene/store/DirectoryCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springelastic.core.lucene.store;
+
+/**
+ * @see org.apache.lucene.store.Directory
+ * @see org.apache.lucene.store.FSDirectory
+ */
+public class DirectoryCore {
+}
